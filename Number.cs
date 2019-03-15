6349870f26ca40ffafa65d416b6618973ca653bb
FIN-THE-HUMAN <malyshevi@yandex.ru>
2019-03-15 23:31:08 +0300

    Initial Commit

---
+++ Number.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustTesting
+{
+    public class Number
+    {
+        public int Value { get; private set; }
+        public int NumberSystem { get; private set; }
+
+        public Number()
+        {
+            NumberSystem = 10;
+            Value = 0;
+        }
+
+        public Number(int numberSystem)
+        {
+            NumberSystem = numberSystem > 1 ? numberSystem : 10;
+            Value = 0;
+        }
+
+        public Number(int numberSystem, int value)
+        {
+            NumberSystem = numberSystem > 1 ? numberSystem : 10;
+            Value = value;
+        }
+
+        private int Log(int lbase, int number)
+        {
+            return (int)Math.Log(number, lbase);
+        }
+
+        private Dictionary<int, int> getPowers()
+        {
+            int numberSystem = NumberSystem;
+            int number = Value;
+            Dictionary<int, int> result = new Dictionary<int, int>();
+            int i = 0;
+            int value = number;
+            while (number >= numberSystem)
+            {
+                i = Log(numberSystem, number);
+                int poweredNumberSystem = (int)Math.Pow(numberSystem, i);
+                value = number / poweredNumberSystem;
+                number = number - poweredNumberSystem * value;
+                result.Add(i, value);
+            }
+            result.Add(0, number);
+
+            return result;
+        }
+
+        private char getSimbolNumber(int value)
+        {
+            if (value <= 9 && value >= 0) return Convert.ToChar(value.ToString());
+            return Convert.ToChar(Convert.ToInt32('A') + value - 10);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            var powers = getPowers();
+            int j = 0;
+            for(int i = powers.First().Key; i >= 0; i--)
+            {
+                if(powers.GetPairByIndex(j).Key == i)
+                {
+                    sb.Append($"{getSimbolNumber(powers.GetPairByIndex(j).Value)}");
+                    j++;
+                }
+                else
+                {
+                    sb.Append("0");
+                }
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Numerics;
+using MatrixLibrary;
+using System.Collections.Generic;
+
+namespace JustTesting
+{
+    class Program
+    {
+        //public static int Log(int lbase, int number)
+        //{
+        //    if (lbase <= 1  || number < 1 || lbase > number && number != 1) return -1; //invalid
+        //    if (number == 1) return 0;
+        //    int result = 0;
+        //    int _number = 1;
+        //    while (number >= _number)
+        //    {
+        //        _number *= lbase;
+        //        result++;
+        //    }
+        //    return result - 1;
+        //}
+
+        public static int Log(int lbase, int number)
+        {
+            return (int)Math.Log(number, lbase);
+        }
+
+        public static Dictionary<int, int> getPowers(Number n)
+        {
+            int numberSystem = n.NumberSystem;
+            int number = n.Value;
+            Dictionary<int, int> result = new Dictionary<int, int>();
+            int i = 0;
+            int value = number;
+            while(number > numberSystem)
+            {
+                i = Log(numberSystem, number);
+                int poweredNumberSystem = (int)Math.Pow(numberSystem, i);
+                value = number / poweredNumberSystem;
+                number = number - poweredNumberSystem * value;
+                result.Add(i, value);  
+            }
+            result.Add(0, number);
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            var n = new Number(16, 64 + 36);
+            Console.WriteLine(n);
+            Console.ReadKey();
+        }
+    }
+}
